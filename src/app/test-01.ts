e1b6f6ae513058b5112c8ef1c6a5334cae48d54a
Raphael <raphael.ongubo@metropol.co.ke>
2022-10-01 13:23:56 +0300

    Update Test 1

--- src/app/test-01.ts
+++ src/app/test-01.ts
@@ -5,33 +5,42 @@
  * * Both [monthly_payment] and [late_payment] should print in the template in currency format : $1,234
  */
 
-import { Component, Input,NgModule  } from '@angular/core';
+import { Component, Input, NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
 @Component({
-    selector : 'ng-app',
-    template : `<div>
-                    <h2>Loan Details</h2>
-                    <b>Monthly Payment:</b> {{monthly_payment}} <br/>
-                    <b>Late Payment Fee : {{late_payment}}</b> <br/>
-                </div>`
+  selector: "ng-app",
+  template: `<div>
+    <h2>Loan Details</h2>
+    <b>Monthly Payment:</b> {{ monthly_payment || "N/A" }}<br />
+    <b>Late Payment Fee : {{ late_payment || "N/A" }}</b> <br />
+  </div>`,
 })
 export class Test01Component {
+  loan_amount: number = 0;
+  monthly_payment: number = 20000;
+  late_payment = 10000;
 
-    loan_amount:number = 1000;
-    monthly_payment:number = 200;
-    late_payment = 10;
+  ngOnInit(): void {
+    if (this.loan_amount == 0 || this.loan_amount == null) {
+      this.monthly_payment = null;
+      this.late_payment = null;
+    } else if (this.loan_amount != null) {
+      this.monthly_payment = 0.02 * this.loan_amount;
+      this.late_payment = 0.05 * this.loan_amount;
+    }
+  }
 }
 
 @NgModule({
-    imports : [
-        RouterModule.forChild([
-            {
-                path : "",
-                component : Test01Component
-            }
-        ])
-    ],
-    declarations : [Test01Component]
+  imports: [
+    RouterModule.forChild([
+      {
+        path: "",
+        component: Test01Component,
+      },
+    ]),
+  ],
+  declarations: [Test01Component],
 })
 export class Test01Module {}
