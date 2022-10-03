5d59ede546762b3bcc3fda07aace1e58bb91cfa6
Raphael <raphael.ongubo@metropol.co.ke>
2022-10-03 08:17:21 +0300

    Fix text 5

--- src/app/test-05.ts
+++ src/app/test-05.ts
@@ -2,86 +2,78 @@
  * Fix the following issues in the component :
  * * ExpressionChangedAfterItHasBeenCheckedError
  * * Spot the memory leak
- * 
+ *
  */
-import { Component, NgModule, Injectable, Input  } from '@angular/core';
-import { RouterModule, Router} from "@angular/router";
-import { CommonModule } from '@angular/common';
-import { BehaviorSubject } from 'rxjs';
-
+import { Component, NgModule, Injectable, Input } from "@angular/core";
+import { RouterModule, Router } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable()
 export class TestService {
-    test:BehaviorSubject<string>;
+  test: BehaviorSubject<string>;
 
-    constructor() {
-        this.test  = new BehaviorSubject("angular test #5");
-    }
+  constructor() {
+    this.test = new BehaviorSubject("angular test #5");
+  }
 
-    SetTest(test:string) {
-        this.test.next(test);
-    }
+  SetTest(test: string) {
+    this.test.next(test);
+  }
 }
 
 @Component({
-    selector : 'ng-app',
-    template : `
-                <h2>Current test is:</h2>
-                {{test}}
-                <br/>
-                <child [skip-current]="true"></child>
-                `,
-    styles : []
+  selector: "ng-app",
+  template: `
+    <h2>Current test is:</h2>
+    {{ test }}
+    <br />
+    <child [skip-current]="true"></child>
+  `,
+  styles: [],
 })
 export class MainComponent {
-    test:string = null;
+  test: string = null;
 
-    constructor(private _srv:TestService) {
+  constructor(private _srv: TestService) {}
 
-    }
-
-    ngOnInit() {
-        
-        this._srv.test.subscribe(test=>{
-            this.test = test;
-        });
-    }
+  ngOnInit() {
+    this._srv.test.subscribe((test) => {
+      this.test = test;
+    });
+  }
 }
 
 @Component({
-    selector : 'child',
-    template : `Sample Child component<br/> <button (click)="Next()">next test</button>`
-    
+  selector: "child",
+  template: `Sample Child component<br />
+    <button (click)="Next()">next test</button>`,
 })
 export class TextChildComponent {
-    
-    @Input('skip-current') skip = false;
+  @Input("skip-current") skip = false;
 
-    constructor(private _srv:TestService, private _router:Router) {
+  constructor(private _srv: TestService, private _router: Router) {
+    if (this.skip) this._srv.SetTest("angular test #6");
+  }
 
-    }
+  Next() {
+    this._router.navigate(["test-six"]);
+  }
 
-    Next() {
-        this._router.navigate(["test-six"]);
-    }
-
-    ngAfterViewInit() {
-        if(this.skip) this._srv.SetTest("angular test #6");
-    }
-
+  ngAfterViewInit() {}
 }
 
 @NgModule({
-    imports : [
-        CommonModule,
-        RouterModule.forChild([
-            {
-                path : "",
-                component : MainComponent
-            }
-        ])
-    ],
-    declarations : [MainComponent,TextChildComponent],
-    providers : [TestService]
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: "",
+        component: MainComponent,
+      },
+    ]),
+  ],
+  declarations: [MainComponent, TextChildComponent],
+  providers: [TestService],
 })
-export class MainModule {};
+export class MainModule {}
